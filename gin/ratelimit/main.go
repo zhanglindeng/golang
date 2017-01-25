ab89b8d647c5af7f0ff1530ab1024c6723417c4f
zhanglindeng <zhanglindeng@outlook.com>
2017-01-25 20:12:13 +0800

    rate limit

---
+++ gin/ratelimit/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/didip/tollbooth"
+	"time"
+	"./middleware"
+	"github.com/gin-gonic/gin"
+)
+
+//func HelloHandler(w http.ResponseWriter, req *http.Request) {
+//	w.Write([]byte("Hello, World!"))
+//}
+//
+//func main() {
+//	// Create a request limiter per handler.
+//	http.Handle("/", tollbooth.LimitFuncHandler(tollbooth.NewLimiter(1, 1* time.Second), HelloHandler))
+//	http.ListenAndServe(":12345", nil)
+//}
+
+func main() {
+	r := gin.New()
+
+	// Create a limiter struct.
+	limiter := tollbooth.NewLimiter(1, time.Second)
+	r.GET("/", middleware.LimitHandler(limiter), func(c *gin.Context) {
+		c.String(200, "Hello, world!")
+	})
+
+	r.Run(":12345")
+}
