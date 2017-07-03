48cc4ca7f1369686af87c5be83983e1a7258dac3
zhanglindeng <zhanglindeng@outlook.com>
2017-07-03 18:11:17 +0800

    Create date_test.go

---
+++ phpgo/date_test.go
@@ -0,0 +1,64 @@
+package phpgo
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestDateDate4(t *testing.T) {
+	s := DateDate("U", 1499075877)
+	t.Log("DateDate", s)
+}
+
+func TestDateDate3(t *testing.T) {
+	t.Log(fmt.Sprintf("%d", 1499075877))
+	t.Logf("%d", 1499075877)
+	s := DateDate("g G u e O P c r U", 1499075877)
+	t.Log(time.Now().Month().String())
+	t.Log(time.Now().Unix())
+	t.Log("DateDate", s)
+}
+
+func TestDateDate2(t *testing.T) {
+	s := DateDate("M n y a A g", 1482278400)
+	t.Log(time.Now().Month().String())
+	t.Log("DateDate", s)
+}
+
+func TestDateDate1(t *testing.T) {
+	t.Log(time.Now().Weekday().String())
+	t.Log(time.Now().YearDay())
+	s := DateDate("Y-m-d H:i:s D j l", 0)
+	t.Log("DateDate", s)
+}
+
+func TestDateDate(t *testing.T) {
+	s := DateDate("Y-m-d H:i:s", 0)
+	t.Log("DateDate", s)
+}
+
+func TestDatemdHi(t *testing.T) {
+	s := DatemdHi()
+	t.Log("DatemdHi", s)
+}
+
+func TestDateYmdHi(t *testing.T) {
+	s := DateYmdHi()
+	t.Log("DateYmdHi", s)
+}
+
+func TestDateYmd(t *testing.T) {
+	s := DateYmd()
+	t.Log("DateYmd", s)
+}
+
+func TestDateHis(t *testing.T) {
+	s := DateHis()
+	t.Log("DateHis", s)
+}
+
+func TestDateYmdHis(t *testing.T) {
+	s := DateYmdHis()
+	t.Log("DateYmdHis", s)
+}
