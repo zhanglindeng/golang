2ab8749843238772ee1c59fc30cd9f3ea76c7155
zhanglindeng <zhanglindeng@outlook.com>
2017-07-18 23:34:28 +0800

    CheckMAC

--- pkg/common/str.go
+++ pkg/common/str.go
@@ -1,1 +1,15 @@
 package common
+
+import (
+  "crypto/hmac"
+  "crypto/sha256"
+)
+
+// CheckMAC reports whether messageMAC is a valid HMAC tag for message.
+// @link https://golang.org/pkg/crypto/hmac/
+func CheckMAC(message, messageMAC, key []byte) bool {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	return hmac.Equal(messageMAC, expectedMAC)
+}
